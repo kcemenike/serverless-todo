2c44893aeecdfb85419aceecea7f4a1b7b599eb6
KC <kcemenike@github.com>
2022-07-29 19:30:22 -0300

    backend deploy

---
+++ backend/src/lambda/http/deleteTodo.ts
@@ -0,0 +1,34 @@
+import 'source-map-support/register'
+
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda'
+
+import { deleteTodo } from '../../businessLogic/todos'
+import { getUserId } from '../utils'
+
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('deleteTodo')
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  // TODO: Remove a TODO item by id
+  // const authorization = event.headers.Authorization
+  logger.info('deleteTodo event')
+
+  const userId = getUserId(event)
+  const todoId = event.pathParameters.todoId
+
+  await deleteTodo(userId, todoId)
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: 'success'
+  }
+}
